19e136e54907cc71a352b5ecbede14135422ad57
Aaron Marburg <amarburg@apl.washington.edu>
2016-06-07 16:12:13 -0700

    Trying libudev.

--- temperature/tools/temperature.cpp
+++ temperature/tools/temperature.cpp
@@ -10,6 +10,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <libudev.h>
+
 #include <mutex>
 #include <thread>
 
@@ -38,9 +40,47 @@ void readTemperature( void )
 	{
 		LockGuard guard( _mutex );
 		if( ! bmp280.initialize() ) {
-			cerr << "Couldn't initiaize BMP280" << endl;
+			cerr << "Couldn't initialize BMP280" << endl;
 			return;
 		}
+	}
+
+	// And initialize libudev
+	struct udev *udev;
+		struct udev_enumerate *enumerate;
+		struct udev_list_entry *devices, *dev_list_entry;
+		struct udev_device *dev;
+
+		/* Create the udev object */
+		udev = udev_new();
+		if (!udev) {
+			cerr << "Can't create udev";
+			return;
+		}
+
+		enumerate = udev_enumerate_new(udev);
+			udev_enumerate_add_match_subsystem(enumerate, "thermal");
+			udev_enumerate_scan_devices(enumerate);
+			devices = udev_enumerate_get_list_entry(enumerate);
+			/* For each item enumerated, print out its information.
+			   udev_list_entry_foreach is a macro which expands to
+			   a loop. The loop will be executed for each member in
+			   devices, setting dev_list_entry to a list entry
+			   which contains the device's path in /sys. */
+			udev_list_entry_foreach(dev_list_entry, devices) {
+				const char *path;
+				/* Get the filename of the /sys entry for the device
+				   and create a udev_device object (dev) representing it */
+				path = udev_list_entry_get_name(dev_list_entry);
+				dev = udev_device_new_from_syspath(udev, path);
+
+				/* usb_device_get_devnode() returns the path to the device node
+				   itself in /dev. */
+				printf("Device Node Path: %s\n", udev_device_get_devnode(dev));
+
+				printf("  type/temp: %s %s\n",
+						        udev_device_get_sysattr_value(dev,"type"),
+						        udev_device_get_sysattr_value(dev, "temp"));
 	}
 
 	while( !_stopping )
@@ -55,6 +95,10 @@ void readTemperature( void )
 
 			std::this_thread::sleep_until( now + std::chrono::seconds(1) );
 	}
+
+	udev_enumerate_unref(enumerate);
+
+	udev_unref(udev);
 }
 
 
