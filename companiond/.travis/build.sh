5919aec22257ee557e3daab9089f5a2dd33530b8
Aaron Marburg <amarburg@apl.washington.edu>
2016-06-08 11:57:35 -0700

    Shonky first go at Travis integration.

---
+++ companiond/.travis/build.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env sh
+
+set -ev
+
+BUILD_TYPE=${BUILD_TYPE:=Release}
+
+if [ ! -d build_ci ]; then mkdir build_ci; fi
+
+cmake -Bbuild_ci -H. -DCMAKE_BUILD_TYPE:string=$BUILD_TYPE
+cd build_ci && make 
