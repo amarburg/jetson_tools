be7d8f582159f9cc86c66d45f8d174ac5c7de469
Aaron Marburg <amarburg@apl.washington.edu>
2016-06-08 12:15:52 -0700

    Migrated all of the build.sh variables to companiond/

--- companiond/.travis/build.sh
+++ companiond/.travis/build.sh
@@ -4,7 +4,7 @@ set -ev
 
 BUILD_TYPE=${BUILD_TYPE:=Release}
 
-if [ ! -d build_ci ]; then mkdir build_ci; fi
+if [ ! -d companiond/build_ci ]; then mkdir companiond/build_ci; fi
 
-cmake -Bbuild_ci -H. -DCMAKE_BUILD_TYPE:string=$BUILD_TYPE
-cd build_ci && make 
+cmake -Bcompaniond/build_ci -Hcompaniond -DCMAKE_BUILD_TYPE:string=$BUILD_TYPE
+cd companiond/build_ci && make 
